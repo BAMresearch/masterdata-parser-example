61b08187133057129aa188f66ddcefb761fd21b7
jpizarro <jose.pizarro-blanco@bam.de>
2025-08-05 15:46:03 +0200

    Fixed tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -7,8 +7,8 @@ class TestMasterdataParserExample:
         collection = CollectionType()
         parser.parse([], collection, logger)
 
-        assert len(collection.objects) == 2
-        objects = list(collection.objects.values())
+        assert len(collection.attached_objects) == 2
+        objects = list(collection.attached_objects.values())
         assert objects[0].name == "Example Chemical"
         assert objects[1].name == "Example Instrument"
         assert len(collection.relationships) == 1
